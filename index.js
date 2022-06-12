d5519d029db8da45948c6ca89a39d973ec2419c2
Will <swilson.carlson@hotmail.com>
2022-06-12 10:59:52 -0400

    commit tree d0e44091ecdbc7d054cee0bd3a1b9acc8e793a1b

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Code your solutions in this file
+
+/*
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+  }
+*/
+
+/*
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+*/
+
+/*
+
+Assignment 1
+
+const stringArray = ["Samwise", "Frodo", "Smeagull"];
+
+let eventName = "graduation";
+
+function writeCards(stringArray, eventName) {
+    let newArray = [];
+    for (let i = 0; i < stringArray.length; i++) {
+
+        let msg = `Thank you, ${stringArray[i]}, for the wonderful ${eventName} gift!`
+        newArray.push(msg);
+    }
+    return newArray;
+}
+
+*/
+
+/*
+
+Assignment 2
+
+*/
+
+function countDown(posInt) {
+    let i = posInt;
+    
+    while (i>=0) {console.log(i);
+    i--;
+    }
+    return posInt;
+}
+
+countDown(10);
